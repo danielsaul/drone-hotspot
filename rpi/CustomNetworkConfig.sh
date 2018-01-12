36818339b872dcc3d7d3d8dec54dc8f1f682eaa6
Viet Cuong Vu <viet.vu.14@ucl.ac.uk>
2018-01-12 15:40:58 +0000

    file to be placed in /etc/init.d/

---
+++ rpi/CustomNetworkConfig.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+chmod 755 /etc/init.d/CustomNetworkConfig.sh
+chmod a+x /etc/init.d/CustomNetworkConfig.sh
+chmod 777 /etc/init.d/CustomNetworkConfig.sh
+
+chmod +x ./ppp-creator.sh
+sudo ./ppp-creator.sh EVERYWHERE ttyUSB3
+sudo pppd call gprs&
+sleep 5
+sudo route add default dev ppp0
+
+sudo sed -i '/denyinterfaces/s/^/#/' /etc/dhcpcd.conf
+sudo service dhcpcd restart
+sleep 10
+sudo service dnsmasq start
+sudo sed -i '/^#.*denyinterfaces /s/^#//' /etc/dhcpcd.conf
