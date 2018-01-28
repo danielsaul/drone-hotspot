64c47970c737d7f43e54bec0fea80119423a05e1
Daniel Saul <daniel@dansaul.co.uk>
2018-01-28 20:04:29 +0000

    Reducer for drone status

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
   connection: require('./connection').default,
+  drone: require('./drone').default,
   location: require('./location').default,
 })
 
