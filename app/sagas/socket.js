89feed26a5faa60f737033620b4b89f58008e18d
Daniel Saul <daniel@dansaul.co.uk>
2018-01-29 16:17:19 +0000

    Add send cases

--- app/sagas/socket.js
+++ app/sagas/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 import { eventChannel, delay } from 'redux-saga';
-import { takeEvery, take, put, call, race, fork } from 'redux-saga/effects';
+import { takeEvery, take, put, call, race, fork, actionChannel } from 'redux-saga/effects';
 
 import { DRONE_CONNECTED, DRONE_DISCONNECTED } from '../reducers/connection';
 import { UPDATE_DRONE } from '../reducers/drone';
@@ -76,16 +76,42 @@ const listen = function* (socket) {
 
 // send
 const send = function* (socket) {
-  /*while (true) {
-    continue; 
-  }*/
+  const chan = yield actionChannel('*');
+  while (true) {
+    action = yield take(chan);
+    switch (action.type){
+      case 'BTN_TAKEOFF':
+        console.log('takeoff');
+        break;
+      case 'BTN_LAND':
+        console.log('land');
+        break;
+      case 'BTN_RETURN':
+        console.log('return');
+        break;
+      case 'UPDATE_LOCATION':
+        console.log('location');
+        break;
+      case 'SET_MODE':
+        console.log('mode');
+        break;
+      case 'UPDATE_MANUAL':
+        console.log('manual');
+        break;
+      case 'UPDATE_FLYTOPOINT':
+        console.log('flytopoint');
+        break;
+    };
+  }
+};
+
+const sendAbort = function* (socket, action) {
+  console.log('abort');
 };
 
 // Saga
 const listenServerSaga = function* () {
   try {
-    
-    console.log("saga");
     // Try to connect
     const {timeout} = yield race({
       connected: call(connect),
@@ -106,6 +132,7 @@ const listenServerSaga = function* () {
     // Listen and Send 
     yield fork(listen, socket);
     yield fork(send, socket);
+    yield takeEvery('BTN_ABORT', sendAbort, socket);
 
   } catch (error) {
     console.log(error);
