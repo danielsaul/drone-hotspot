afa527c3f499cd4482de06db3749fa0e4920a991
Daniel Saul <daniel@dansaul.co.uk>
2018-02-20 20:43:19 +0000

    Push received commands through pipe

--- server/drone_control/socket.py
+++ server/drone_control/socket.py
@@ -4,6 +4,8 @@ import eventlet
 sio = socketio.Server(async_mode='eventlet')
 app = socketio.Middleware(sio)
 
+pipe = None
+
 def background_loop(drone_state):
     while True:
         sio.sleep(0.5)
@@ -17,6 +19,38 @@ def emit_drone_status(state):
 
     sio.emit('updateDroneStatus', state)
 
-def start(drone_state):
+@sio.on('action')
+def action(sid, data):
+    global pipe
+    pipe.send(('action', data))
+
+@sio.on('update_all')
+def update_all(sid, data):
+    global pipe
+    pipe.send(('update_all', data))
+
+@sio.on('update_location')
+def update_location(sid, data):
+    global pipe
+    pipe.send(('update_location', data))
+
+@sio.on('set_mode')
+def set_mode(sid, data):
+    global pipe
+    pipe.send(('set_mode', data))
+
+@sio.on('update_manual')
+def update_manual(sid, data):
+    global pipe
+    pipe.send(('update_manual', data))
+
+@sio.on('update_flytopoint')
+def update_flytopoint(sid, data):
+    global pipe
+    pipe.send(('update_flytopoint', data))
+
+def start(drone_state, pipe_end):
+    global pipe
+    pipe = pipe_end
     sio.start_background_task(background_loop, drone_state)
     eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
--- server/test/test_socket.py
+++ server/test/test_socket.py
@@ -1,7 +1,82 @@
 import unittest
-from app_comms.socket import *
+import mock
+from drone_control.socket import *
+
+from multiprocessing import Manager, Pipe
 
 class TestSocket(unittest.TestCase):
     def setUp(self):
         pass
+
+    def test_emitDroneStatus_WrongState(self):
+        with self.assertRaises(ValueError):
+            emit_drone_status({})
+            emit_drone_status({'status': "test", altitude: 5})
+
+    @mock.patch('socketio.Server.emit')
+    def test_emitDroneStatus_CorrectState(self, emit):
+        state = {
+            'status': "flying",
+            'location': {
+                'latitude': 0.003394,
+                'longitude': 45345345.0,
+            },
+            'altitude': 323,
+            'distance': 54,
+            'speed': 234,
+            'battery': 8,
+            'signal': 4
+        }
+
+        emit_drone_status(state)
+        emit.assert_called_once_with('updateDroneStatus', state)
+    
+    @mock.patch('eventlet.wsgi.server')
+    @mock.patch('socketio.Server.start_background_task')
+    def test_start_background_loop(self, bg, server):
+        manager = Manager()
+        state = manager.dict()
+        state['test'] = 'test'
+
+        start(state, None)
+        bg.assert_called_once_with(background_loop, state)
+        server.assert_called_once()
+
+    @mock.patch('drone_control.socket.pipe')
+    def test_action_sentoverpipe(self, pipe):
+        action(0, 'takeoff')
+        pipe.send.assert_called_once_with(('action', 'takeoff'))
+
+    @mock.patch('drone_control.socket.pipe')
+    def test_updateall_sentoverpipe(self, pipe):
+        update_all(0, {'test': 'test'})
+        pipe.send.assert_called_once_with(('update_all', {'test': 'test'}))
+
+    @mock.patch('drone_control.socket.pipe')
+    def test_updatelocation_sentoverpipe(self, pipe):
+        update_location(0, {'test': 'test'})
+        pipe.send.assert_called_once_with(('update_location', {'test': 'test'}))
+
+    @mock.patch('drone_control.socket.pipe')
+    def test_setmode_sentoverpipe(self, pipe):
+        set_mode(0, {'test': 'test'})
+        pipe.send.assert_called_once_with(('set_mode', {'test': 'test'}))
+
+    @mock.patch('drone_control.socket.pipe')
+    def test_updatemanual_sentoverpipe(self, pipe):
+        update_manual(0, {'test': 'test'})
+        pipe.send.assert_called_once_with(('update_manual', {'test': 'test'}))
+
+    @mock.patch('drone_control.socket.pipe')
+    def test_updateflytopoint_sentoverpipe(self, pipe):
+        update_flytopoint(0, {'test': 'test'})
+        pipe.send.assert_called_once_with(('update_flytopoint', {'test': 'test'}))
+
+
+
+
+
+
+
+
 
