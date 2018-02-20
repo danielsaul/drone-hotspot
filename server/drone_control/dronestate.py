edabf643c16135f28697066d86281eac78aa5186
Daniel Saul <daniel@dansaul.co.uk>
2018-02-20 12:45:31 +0000

    Initial drone state and tests

--- server/drone_control/dronestate.py
+++ server/drone_control/dronestate.py
@@ -2,3 +2,18 @@
 class DroneState(object):
     def __init__(self):
         self.state = {}
+        self.reset_state()
+
+    def reset_state(self):
+        self.state = {
+            'status': "waiting",
+            'location': {
+                'latitude': None,
+                'longitude': None
+            },
+            'altitude': None,
+            'distance': None,
+            'speed': None,
+            'battery': None,
+            'signal': None
+        }
--- server/test/test_dronestate.py
+++ server/test/test_dronestate.py
@@ -5,9 +5,38 @@ class TestDroneState(unittest.TestCase):
     def setUp(self):
         self.func = DroneState()
 
-    def test_true(self):
-        self.assertTrue(True)
+    def test_initial_state(self):
+        state = {
+            'status': "waiting",
+            'location': {
+                'latitude': None,
+                'longitude': None
+            },
+            'altitude': None,
+            'distance': None,
+            'speed': None,
+            'battery': None,
+            'signal': None
+        }
+        self.assertEqual(self.func.state, state)
 
+    def test_reset_state(self):
+        state = {
+            'status': "flying",
+            'location': {
+                'latitude': 0.003394,
+                'longitude': 45345345.0,
+            },
+            'altitude': 323,
+            'distance': 54,
+            'speed': 234,
+            'battery': 8,
+            'signal': 4
+        }
+        self.func.state = state
+        self.assertEqual(self.func.state, state)
+        self.func.reset_state()
+        self.test_initial_state()
 
 if __name__ == '__main__':
     unittest.main()
