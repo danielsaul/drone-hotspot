196c3015f65f88911a87cee4ee79648731b98c0e
Daniel Saul <daniel@dansaul.co.uk>
2018-02-24 17:25:36 +0000

    Start serial

--- server/ec25_modem/ec25.py
+++ server/ec25_modem/ec25.py
@@ -1,5 +1,17 @@
 import serial
 
 class Modem(object):
-    def __init__(self):
-        pass
+    def __init__(self, port):
+        self.port = port
+        self.ser = None
+        
+        self.ser_connected = False
+
+    def start(self):
+        try:
+            self.ser = serial.Serial(self.port, 115200, timeout=1)
+            self.ser_connected = True
+        except serial.SerialException:
+            print "EC25 serial port error connecting: %s" % self.port
+
+        return self.ser_connected
--- server/test/test_ec25.py
+++ server/test/test_ec25.py
@@ -1,8 +1,27 @@
 import unittest
 import mock
-
+import serial
 from ec25_modem.ec25 import Modem
 
 class TestModem(unittest.TestCase):
     def setUp(self):
-        self.m = Modem()
+        self.port = "/dev/testport"
+        self.m = Modem(self.port)
+
+    @mock.patch('ec25_modem.ec25.serial.Serial')
+    def test_start_serialsuccess(self, s):
+        res = self.m.start()
+
+        s.assert_called_once_with(self.port, 115200, timeout=1)
+        self.assertEquals(res, True)
+        self.assertEquals(self.m.ser_connected, True)
+
+    @mock.patch('ec25_modem.ec25.serial.Serial')
+    def test_start_serialfail(self, s):
+        s.side_effect = serial.SerialException()
+
+        res = self.m.start()
+
+        s.assert_called_once_with(self.port, 115200, timeout=1)
+        self.assertEquals(res, False)
+        self.assertEquals(self.m.ser_connected, False)
