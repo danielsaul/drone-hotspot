33ef2a3624d0776e37b4ab70ffcd64b33c781310
Daniel Saul <daniel@dansaul.co.uk>
2018-03-04 15:36:45 +0000

    Add sane default vals

--- server/drone_control/controlstate.py
+++ server/drone_control/controlstate.py
@@ -6,18 +6,18 @@ class ControlState(object):
 
     def reset_state(self):
         self.state = {
-            'mode': None,
+            'mode': "manual",
             'location': {
                 'latitude': None,
                 'longitude': None
             },
             'manual': {
                 'move': {
-                    'x': None,
-                    'y': None
+                    'x': 0.0,
+                    'y': 0.0
                 },
-                'altitude': None,
-                'yaw': None
+                'altitude': 0.0,
+                'yaw': 0.0
             },
             'flytopoint': {
                 'altitude': None,
